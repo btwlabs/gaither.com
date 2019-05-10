143f4648315920b4e15aefa29a0bcdaf4d9181f6
Ted Benice <ted@bythewaylabs.com>
2019-05-10 14:51:25 -0700

    trying to get afterinstall script to run

---
+++ scripts/fixperms.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /media/efs/platform-test/acadia2-0916;
+echo Changing owner of installation...;
+sudo chown -R ec2-user:nginx .;
+echo "done";
+echo Changing permissions on all directories...
+find . -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+echo "done";
+echo Changing permissions on all files...;
+find . -type f -exec chmod u=rw,g=r,o= '{}' \;
+echo "done";
+
+echo Changing permissions on all directories in files folder;
+cd /media/efs/platform-test/acadia2-0916/sites;
+find . -type d -name files -exec chmod ug=rwx,o= '{}' \;
+for d in ./*/files
+do
+   find $d -type d -exec chmod ug=rwx,o= '{}' \;
+   find $d -type f -exec chmod ug=rw,o= '{}' \;
+done
+echo "done" ;
+echo ALL DONE!;
