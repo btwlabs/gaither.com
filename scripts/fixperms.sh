db18963b4486202fbb40cf850703ba011cc798e5
Ted Benice <ted@bythewaylabs.com>
2019-05-10 17:06:09 -0700

    change the cd in permissions fixer script

--- scripts/fixperms.sh
+++ scripts/fixperms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /media/efs/platform-test/acadia2-0916;
+cd /media/efs/platforms/acadia2-0916;
 echo Changing owner of installation...;
 sudo chown -R ec2-user:nginx ./;
 echo "done";
@@ -12,7 +12,7 @@ find ./ -type f -exec chmod u=rw,g=r,o= '{}' \;
 echo "done";
 
 echo Changing permissions on all directories in files folder;
-cd /media/efs/platform-test/acadia2-0916/sites;
+cd /media/efs/platforms/acadia2-0916/sites;
 find ./ -type d -name files -exec chmod ug=rwx,o= '{}' \;
 for d in ./*/files
 do
