149c3e756f07b149e1f34deabddaca03d8e70a34
Ted Benice <ted@bythewaylabs.com>
2019-05-10 17:51:49 -0700

    dont need to change perms on files folder as we wont be messing with it

--- scripts/fixperms.sh
+++ scripts/fixperms.sh
@@ -9,15 +9,5 @@ find ./ -type d -exec chmod u=rwx,g=rx,o= '{}' \;
 echo "done";
 echo Changing permissions on all files...;
 find ./ -type f -exec chmod u=rw,g=r,o= '{}' \;
-echo "done";
-
-echo Changing permissions on all directories in files folder;
-cd /media/efs/platforms/acadia2-0916/sites;
-find ./ -type d -name files -exec chmod ug=rwx,o= '{}' \;
-for d in ./*/files
-do
-   find $d -type d -exec chmod ug=rwx,o= '{}' \;
-   find $d -type f -exec chmod ug=rw,o= '{}' \;
-done
 echo "done" ;
 echo ALL DONE!;
