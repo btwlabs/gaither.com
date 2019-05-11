38e5bb8bce6a61404dad351b8d27721e0765f00e
Ted Benice <ted@bythewaylabs.com>
2019-05-11 07:51:57 -0700

    fix perm change deployment script to not follow symlinks on chown

--- scripts/fixperms.sh
+++ scripts/fixperms.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 cd /media/efs/platforms/acadia2-0916;
+
 echo Changing owner of installation...;
-sudo chown -R ec2-user:nginx ./;
+sudo chown -R -h ec2-user:nginx ./;
 echo "done";
-echo Changing permissions on all directories...
+
+echo Changing permissions on all directories...;
 find ./ -type d -exec chmod u=rwx,g=rx,o= '{}' \;
 echo "done";
+
+
 echo Changing permissions on all files...;
 find ./ -type f -exec chmod u=rw,g=r,o= '{}' \;
 echo "done" ;
+
 echo ALL DONE!;
